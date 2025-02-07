6f5dd11869a1dff25d9d257c76371a981b464bac
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2025-02-07 08:59:44 +0100

    Simplify supabase client initialization.
    Removed unnecessary variable assignments and console logs. Directly passed environment variables to `createClient` for cleaner and more concise code.

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -2,12 +2,4 @@ import { createClient } from '@supabase/supabase-js'
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const SUPABASE_URL = process.env.SUPABASE_URL
-const SUPABASE_KEY = process.env.SUPABASE_KEY
-
-console.log(SUPABASE_URL)
-console.log(SUPABASE_KEY)
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY,{
-    auth: { persistSession: false },
-})
+export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
