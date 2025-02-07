fea6f44ed5f2c6e7ac306a274c9995a1101a6602
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2025-02-07 08:51:49 +0100

    Add debug logs for Supabase URL and key
    This commit adds console.log statements to output the Supabase URL and key. These logs are intended for debugging purposes to verify environment variable values during initialization.

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -5,6 +5,9 @@ dotenv.config()
 const SUPABASE_URL = process.env.SUPABASE_URL
 const SUPABASE_KEY = process.env.SUPABASE_KEY
 
+console.log(SUPABASE_URL)
+console.log(SUPABASE_KEY)
+
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY,{
     auth: { persistSession: false },
 })
